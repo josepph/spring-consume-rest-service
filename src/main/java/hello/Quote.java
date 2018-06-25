1ae7a8b58f651c69b5e857e62c6f42145a5a072a
PepetP <pepet71@gmail.com>
2018-06-25 13:24:53 +0200

    Adding code!

--- src/main/java/hello/Quote.java
+++ src/main/java/hello/Quote.java
@@ -1,4 +1,24 @@
 package hello;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown=true)
 public class Quote {
+
+    private String type;
+    private Value value;
+
+    public Quote() {};
+
+    @Override
+    public String toString() {
+        return "Quote{" +
+                "type='" + type + '\'' +
+                ", value=" + value +
+                '}';
+    }
+
+
 }
--- src/main/java/hello/Value.java
+++ src/main/java/hello/Value.java
@@ -1,4 +1,23 @@
 package hello;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown=true)
 public class Value {
+
+    private Long id;
+    private String quote;
+
+    public Value() {};
+
+    public String toString() {
+        return "Value{" +
+                "id='" + id+ '\'' +
+                ", quote=" + quote +
+                '}';
+    }
+
+
 }
